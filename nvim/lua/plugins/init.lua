d2919b7e1bfa3cee44b8d6b5ce0901850452621d
bear <jivko91@gmail.com>
2025-07-25 18:29:20 +0300

    fully switched to modules

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,14 @@
+return {
+    require('plugins.colorizer'),
+    require('plugins.gitsigns'),
+    require('plugins.which-key'),
+    require('plugins.telescope'),
+    require('plugins.lazydev'),
+    require('plugins.nvim-lspconfig'),
+    require('plugins.conform'),
+    require('plugins.blink'),
+    require('plugins.everforest'),
+    require('plugins.todo-comments'),
+    require('plugins.mini'),
+    require('plugins.treesitter'),
+}
