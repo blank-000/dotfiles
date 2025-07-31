88733e8a211079325d7c1df7e4c49ca980aca2fe
bear <jivko91@gmail.com>
2025-07-31 09:25:37 +0300

    Changed out wallpaper naming scheme

--- hypr/scripts/set_wallpaper.sh
+++ hypr/scripts/set_wallpaper.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-one=${HOME}/Pictures/Wallpapers/bear_colors/bear_green.png
-two=${HOME}/Pictures/Wallpapers/bear_colors/bear_yellow.png
-three=${HOME}/Pictures/Wallpapers/bear_colors/bear_aqua.png
-four=${HOME}/Pictures/Wallpapers/bear_colors/bear_orange.png
-five=${HOME}/Pictures/Wallpapers/bear_colors/bear_red.png
+one=${HOME}/Pictures/Wallpapers/bear_colors/one.png
+two=${HOME}/Pictures/Wallpapers/bear_colors/two.png
+three=${HOME}/Pictures/Wallpapers/bear_colors/three.png
+four=${HOME}/Pictures/Wallpapers/bear_colors/four.png
+five=${HOME}/Pictures/Wallpapers/bear_colors/five.png
 
 case "$1" in
-    1) selected="$one" ;;
-    2) selected="$two" ;;
-    3) selected="$three" ;;
-    4) selected="$four" ;;
-    5) selected="$five" ;;
-    *) selected="$one" ;;
+1) selected="$one" ;;
+2) selected="$two" ;;
+3) selected="$three" ;;
+4) selected="$four" ;;
+5) selected="$five" ;;
+*) selected="$one" ;;
 esac
 
 hyprctl hyprpaper wallpaper ",$selected"
